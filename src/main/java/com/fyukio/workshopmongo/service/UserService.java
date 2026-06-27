16b61cdec994d1999720f6783ec5128830f67a20
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-27 19:35:12 -0300

    Insercao de usuario via POST

--- src/main/java/com/fyukio/workshopmongo/service/UserService.java
+++ src/main/java/com/fyukio/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fyukio.workshopmongo.domain.User;
+import com.fyukio.workshopmongo.dto.UserDTO;
 import com.fyukio.workshopmongo.repository.UserRepository;
 import com.fyukio.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User FromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
