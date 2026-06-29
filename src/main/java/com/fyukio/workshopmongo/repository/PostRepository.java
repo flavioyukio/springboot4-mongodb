b48130931dae8d3b674cdb48ec39969976ff2972
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-29 11:56:20 -0300

    Cirando entity Post com User aninhado

---
+++ src/main/java/com/fyukio/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.fyukio.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fyukio.workshopmongo.domain.Post;
+
+@Repository 
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+
+}
