e84bf9b0fbc664551759bdc63fe657a3278e96db
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-06-30 16:54:07 -0300

    Consulta simples com query methods

--- src/main/java/com/fyukio/workshopmongo/domain/resources/PostResource.java
+++ src/main/java/com/fyukio/workshopmongo/domain/resources/PostResource.java
@@ -7,23 +7,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fyukio.workshopmongo.domain.Post;
 import com.fyukio.workshopmongo.service.PostService;
 
 @RestController
-@RequestMapping(value = "users")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
-	@Autowired
-	private PostService service;
+    @Autowired
+    private PostService service;
 
-	
-	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
-	public ResponseEntity<Post> findposts(@PathVariable String id) {
-		Post obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-	}
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(
+            @RequestParam(value = "text", defaultValue = "") String text) {
+
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/fyukio/workshopmongo/service/PostService.java
+++ src/main/java/com/fyukio/workshopmongo/service/PostService.java
@@ -7,19 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fyukio.workshopmongo.domain.Post;
-import com.fyukio.workshopmongo.domain.User;
 import com.fyukio.workshopmongo.repository.PostRepository;
 import com.fyukio.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
 
-	@Autowired
-	private PostRepository repo;
+    @Autowired
+    private PostRepository repo;
 
-	public Post findById(String id) {
-		Optional<Post> user = repo.findById(id);
-		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-	}
+    public Post findById(String id) {
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() ->
+                new ObjectNotFoundException("Objeto não encontrado"));
+    }
 
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
